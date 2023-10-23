c22e1a1a8f91fd8d472daea2c9712d7ea34ba1bf
ysobella <ysobella03@gmail.com>
2023-10-23 20:37:51 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,13 +9,13 @@ const Navbar = () => {
 
         {/* Circular buttons */}
         <div className="flex flex-col items-center mt-auto mb-10">
-          <button className="w-10 h-10 rounded-full bg-white mb-4 text-black">H</button>
-          <button className="w-10 h-10 rounded-full bg-white mb-4 text-black">G</button>
-          <button className="w-10 h-10 rounded-full bg-white mb-4 text-black">F</button>
-          <button className="w-10 h-10 rounded-full bg-white mb-4 text-black">N</button>
-          <button className="w-10 h-10 rounded-full bg-white mb-4 text-black">S</button>
+          <button className="w-7 h-7 rounded-full bg-white mb-4 text-black">H</button>
+          <button className="w-7 h-7 rounded-full bg-white mb-4 text-black">G</button>
+          <button className="w-7 h-7 rounded-full bg-white mb-4 text-black">F</button>
+          <button className="w-7 h-7 rounded-full bg-white mb-4 text-black">N</button>
+          <button className="w-7 h-7 rounded-full bg-white mb-4 text-black">S</button>
           <hr className="border-t my-2 w-8 border-citron" />
-          <button className="w-10 h-10 rounded-full bg-white mt-4 text-black">L</button>
+          <button className="w-7 h-7 rounded-full bg-white mt-4 text-black">L</button>
         </div>
         
       </nav>
