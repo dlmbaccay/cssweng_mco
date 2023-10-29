ff65bd1e91da9952ae2922c725869e1c738674f2
dlmbaccay <dominic_baccay@dlsu.edu.ph>
2023-10-30 06:09:16 +0800

    added testProfile w sample data to avoid quota

---
+++ src/pages/testPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TestUserProfile from '../components/TestUserProfile'
+
+export default function testPage() {
+  return (
+    <div>
+      <TestUserProfile />
+    </div>
+  )
+}
