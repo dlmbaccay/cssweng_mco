87080c9a52c3c694304b6cadae28d4a6de915bea
dlmbaccay <dominic_baccay@dlsu.edu.ph>
2023-11-02 02:13:48 +0800

    quick fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -201,28 +201,8 @@ export default function Login() {
             >
                 <div className="flex flex-col">
                     <PostSnippet
-                        username='Barker'
-                        displayName='barknplay'
-                        publish_date='Sept 6 at 4:30 PM'    
-                        desc='Chaos and cuddles with this dynamic quartet! 🐾🐾🐾🐾 
-                            Our two pups and two kitties bring a whole lot of joy and a touch of mayhem to our everyday life. 
-                            🐶🐱🐶🐱 They may be different species, but they share a bond thats truly heartwarming.'
-                        user_img_src='/images/user1-image.png'
-                        post_img_src='/images/post1-image.png'
-                        style={{ scrollSnapAlign: 'start' }}/>
-                    <PostSnippet
-                        username='Barker'
-                        displayName='barknplay'
-                        publish_date='Sept 6 at 4:30 PM'    
-                        desc='Chaos and cuddles with this dynamic quartet! 🐾🐾🐾🐾 
-                            Our two pups and two kitties bring a whole lot of joy and a touch of mayhem to our everyday life. 
-                            🐶🐱🐶🐱 They may be different species, but they share a bond thats truly heartwarming.'
-                        user_img_src='/images/user1-image.png'
-                        post_img_src='/images/post1-image.png'
-                        style={{ scrollSnapAlign: 'start' }}/>
-                    <PostSnippet
-                        username='Barker'
-                        displayName='barknplay'
+                        username='barknplay'
+                        displayName='Barker'
                         publish_date='Sept 6 at 4:30 PM'    
                         desc='Chaos and cuddles with this dynamic quartet! 🐾🐾🐾🐾 
                             Our two pups and two kitties bring a whole lot of joy and a touch of mayhem to our everyday life. 
