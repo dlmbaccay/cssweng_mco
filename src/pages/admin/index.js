9705c50a7f1ad6355c6be850cb065f951464777b
dlmbaccay <dominic_baccay@dlsu.edu.ph>
2023-11-24 19:19:15 +0800

    Add admin authentication and redirect to admin page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import withSpecialAuth from '@/src/components/withSpecialAuth'
+import { auth } from '@/src/lib/firebase'
+import toast from 'react-hot-toast'
 
-function index() {
+function Admin() {
   return (
-    <div>Admin Landing Page</div>
+    <div className='flex flex-row w-full h-screen overflow-hidden bg-dark_gray'>
+      <button onClick={() => {
+        auth.signOut()
+        .then(() => {
+          toast.success('Admin signed out')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      }}>
+        Admin Log Out
+      </button>
+    </div>
   )
 }
 
-export default withSpecialAuth('KEIay0tvXbf3ha2z20zZpFccpWj1')(index)
+export default withSpecialAuth('luTr6y0B1TUOkimRQlfiCO9xQqo1')(Admin)
