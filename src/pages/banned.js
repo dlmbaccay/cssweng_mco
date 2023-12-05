3d7c03615b345589aad73bf0bad3d0b2e779fe4c
kmmytn <134942694+kmmytn@users.noreply.github.com>
2023-12-05 11:12:48 +0800

    Update banned.js

--- src/pages/banned.js
+++ src/pages/banned.js
@@ -5,16 +5,68 @@ import Router from 'next/router';
 export default function BanPage() {
     const router = Router;
   return (
-    <div class="flex lg:flex-row max-sm:flex-col h-screen px-4 bg-snow justify-center items-center lg:gap-10 max-sm:gap-0">
-      <Image src='/images/error.png' alt='error' width={300} height={300} className='max-sm:h-72 max-sm:w-auto'/>
-      <div className='mt-20 max-sm:text-center max-sm:mt-3'>
-          <h1 class="font-black text-gray-200 text-9xl max-sm:text-5xl">Banned</h1>
-          <p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Oops!</p>
-          <p class="mt-4 max-sm:mt-2 text-raisin_black">You have multiple violations.</p>
-          <button class="w-fit px-5 py-3 mt-6 max-sm:mt-3 text-shining text-sm font-medium text-white   bg-grass rounded-md hover:bg-[#3C5D1A] transition-all focus:outline-none focus:ring"
-                  onClick={() => router.push('/')}>Go back to Login</button>
+    <div className="flex items-center justify-center w-full h-screen overflow-hidden">
+     <div className="flex items-center justify-center w-[700px] h-[500px] overflow-hidden rounded-xl">
+       <div className="relative w-[706px] h-[521px] left-[-6px]">
+         <div className="absolute w-[700px] h-[500px] top-0 left-[6px] bg-jasmine rounded-xl" />
+         <div className="absolute top-[110px] left-[152px] font-medium text-grass text-[40px] tracking-[0] leading-[normal] font-shining p-10 justify-center">
+           You Are Banned due to multiple violations
+         </div>
+
+         <div className="absolute w-[100px] h-[px] top-[420px] left-0">
+           <div className="relative h-[101px]">
+             <div className="absolute w-[104px] h-[16px] top-[43px] left-[-2px] bg-grass rounded-[20px] rotate-[-27.76deg]" />
+             <div className="absolute w-[90px] h-[16px] top-[30px] left-[-2px] bg-grass rounded-[20px] rotate-[65.00deg]" />
+           </div>
+         </div>
+         <div className="absolute w-[100px] h-[101px] top-[288px] left-[494px] rotate-[-30.11deg]">
+           <div className="relative h-[101px]">
+             <div className="absolute w-[104px] h-[16px] top-[43px] left-[-2px] bg-grass rounded-[20px] rotate-[-27.76deg]" />
+             <div className="absolute w-[104px] h-[16px] top-[43px] left-[-2px] bg-grass rounded-[20px] rotate-[65.00deg]" />
+           </div>
+         </div>
+         <div className="absolute w-[52px] h-[53px] top-[165px] left-[84px] rotate-[-30.11deg]">
+           <div className="relative h-[53px]">
+             <div className="absolute w-[54px] h-[8px] top-[22px] -left-px bg-grass rounded-[20px] rotate-[-27.76deg]" />
+             <div className="absolute w-[54px] h-[8px] top-[22px] -left-px bg-grass rounded-[20px] rotate-[65.00deg]" />
+           </div>
+         </div>
+         <div className="absolute w-[52px] h-[53px] top-[70px] left-[564px] rotate-[-11.75deg]">
+           <div className="relative h-[53px]">
+             <div className="absolute w-[54px] h-[8px] top-[22px] -left-px bg-grass rounded-[20px] rotate-[-27.76deg]" />
+             <div className="absolute w-[54px] h-[8px] top-[22px] -left-px bg-grass rounded-[20px] rotate-[65.00deg]" />
+           </div>
+         </div>
+         <div className="absolute w-[46px] h-[41px] top-[300px] left-[171px]">
+           <div className="w-[32px] h-[32px] top-[5px] left-0 rounded-[16px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-0 left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-[16px] left-[37px] rounded-[4.5px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-[32px] left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+         </div>
+         <div className="absolute w-[46px] h-[41px] top-[341px] left-[247px]">
+           <div className="w-[32px] h-[32px] top-[5px] left-0 rounded-[16px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-0 left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-[16px] left-[37px] rounded-[4.5px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-[32px] left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+         </div>
+         <div className="absolute w-[46px] h-[41px] top-[300px] left-[325px]">
+           <div className="w-[32px] h-[32px] top-[5px] left-0 rounded-[16px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-0 left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-[16px] left-[37px] rounded-[4.5px] absolute bg-[#90d17d]" />
+           <div className="w-[9px] h-[9px] top-[32px] left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+         </div>
+         <div className="absolute w-[46px] h-[41px] top-[332px] left-[403px]">
+          <div className="w-[32px] h-[32px] top-[5px] left-0 rounded-[16px] absolute bg-[#90d17d]" />
+          <div className="w-[9px] h-[9px] top-0 left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+          <div className="w-[9px] h-[9px] top-[16px] left-[37px] rounded-[4.5px] absolute bg-[#90d17d]" />
+          <div className="w-[9px] h-[9px] top-[32px] left-[32px] rounded-[4.5px] absolute bg-[#90d17d]" />
+        </div>
+        <div className="absolute top-[350px] left-[280px]">
+        <button class="w-fit px-5 py-3 mt-6 max-sm:mt-3 text-shining text-sm font-medium text-white bg-grass rounded-md hover:bg-[#3C5D1A] transition-all focus:outline-none focus:ring"
+              onClick={() => router.push('/')} style={{position: 'relative', top: '50px'}}>Go back to Login</button>
+        </div>
+        </div>
       </div>
-        
-    </div>
-  )
+      </div>
+  ) 
 }
